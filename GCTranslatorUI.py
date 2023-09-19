4d4693aa6bb9e0ec0086df30d21fd50441d973c7
Brad Wardell <115580821+Draginol@users.noreply.github.com>
2023-09-19 18:09:38 -0400

    Big Italian update

--- GCTranslatorUI.py
+++ GCTranslatorUI.py
@@ -27,6 +27,8 @@ from PyQt5.QtCore import Qt
 from lxml import etree as ET  # Use lxml's ElementTree API
 from concurrent.futures import ThreadPoolExecutor
 
+
+
 import threading
 
 openai.api_key = "Your OPENAI_API_KEY"
@@ -287,7 +289,7 @@ class TranslationApp(QMainWindow):
             response = openai.ChatCompletion.create(
                 model="gpt-4",
                 messages=[{"role": "user", "content": prompt}],
-                max_tokens=250,
+                max_tokens=500,
                 n=1,
                 stop=None,
                 temperature=0.7,
@@ -319,11 +321,11 @@ class TranslationApp(QMainWindow):
             return row, translated_text
 
         # Split the rows into chunks
-        CHUNK_SIZE = 25
+        CHUNK_SIZE = 10
         chunks = [selected_rows[i:i + CHUNK_SIZE] for i in range(0, len(selected_rows), CHUNK_SIZE)]
 
         for chunk in chunks:
-            with ThreadPoolExecutor(max_workers=2) as executor:
+            with ThreadPoolExecutor(max_workers=4) as executor:
                 for idx, (row, translated_text) in enumerate(executor.map(translate_row, chunk), start=translation_counter):
                     translation_item = self.table.item(row, 4)
                     if not translation_item:
