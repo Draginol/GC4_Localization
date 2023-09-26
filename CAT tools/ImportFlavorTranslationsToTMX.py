c8c5be1cf351d95d6b3be6d56dbd3ca1ec66c04a
Brad Wardell <115580821+Draginol@users.noreply.github.com>
2023-09-26 15:08:13 -0400

    Update ImportFlavorTranslationsToTMX.py

--- CAT tools/ImportFlavorTranslationsToTMX.py
+++ CAT tools/ImportFlavorTranslationsToTMX.py
@@ -13,50 +13,71 @@ def build_translation_dict(translated_dir):
             root = ET.fromstring(content)
             for flavor_text_def in root.findall('.//FlavorTextDef'):
                 internal_name = flavor_text_def.find('InternalName').text
-                for idx, text_element in enumerate(flavor_text_def.findall('.//FlavorTextOption/Text')):
-                    key = (os.path.basename(filepath), internal_name, idx + 1)
-                    translation_dict[key] = text_element.text
+                
+                # Loop through each FlavorTextOption
+                for flavor_text_option in flavor_text_def.findall('.//FlavorTextOption'):
+                    requirements = []
+                    for req_element in flavor_text_option.findall('./Requirements/*'):
+                        req_text = ET.tostring(req_element, encoding='utf-8', method='xml').decode('utf-8').strip()
+                        requirements.append(req_text)
+                    req_string = "\n".join(requirements)
+                    
+                    # Loop through each Text element inside the current FlavorTextOption
+                    for idx, text_element in enumerate(flavor_text_option.findall('.//Text')):
+                        key = (internal_name, idx + 1, req_string)
+                        translation_dict[key] = (os.path.basename(filepath), text_element.text)
     return translation_dict
+
+
 
 def update_tmx_with_translation(tmx_path, translations):
     tree = ET.parse(tmx_path)
     root = tree.getroot()
     
+    changes = []
+
     for tu in root.findall('.//tu'):
-        prop_filename = tu.find("./prop[@type='Filename']")
         prop_internal_name = tu.find("./prop[@type='InternalName']")
         prop_text_index = tu.find("./prop[@type='TextIndex']")
+        prop_requirements = tu.find("./prop[@type='Requirements']")
         
-        if prop_filename is not None and prop_internal_name is not None and prop_text_index is not None:
-            key = (prop_filename.text, prop_internal_name.text, int(prop_text_index.text))
+        if prop_internal_name is not None and prop_text_index is not None:
+            req_string = prop_requirements.text if prop_requirements is not None else ""
+            key = (prop_internal_name.text, int(prop_text_index.text), req_string)
             if key in translations:
+                filename, translated_text = translations[key]
                 for tuv in tu.findall('./tuv'):
                     lang = tuv.attrib.get('{http://www.w3.org/XML/1998/namespace}lang')
                     if lang and lang != 'EN':
                         seg_element = tuv.find('./seg')
-                        seg_element.text = translations[key]
+                        changes.append((filename, seg_element.text, translated_text))
+                        seg_element.text = translated_text
                         break
     tree.write(tmx_path, encoding="utf-8", xml_declaration=True)
+    return changes
 
 
 def main():
     root = tk.Tk()
     root.withdraw()
     
-    # TMX file selection
     tmx_path = filedialog.askopenfilename(filetypes=[("TMX files", "*.tmx"), ("All files", "*.*")], title="Select the TMX file to update")
     if not tmx_path:
         print("TMX file selection was canceled.")
         return
     
-    # Directory containing the translated files
     translated_dir = filedialog.askdirectory(title="Select directory containing translated files")
     if not translated_dir:
         print("Directory selection was canceled.")
         return
     
     translations = build_translation_dict(translated_dir)
-    update_tmx_with_translation(tmx_path, translations)
+    changes = update_tmx_with_translation(tmx_path, translations)
+    for filename, old_text, new_text in changes:
+        print(f"File: {filename}")
+        print("Old Translation:", old_text)
+        print("New Translation:", new_text)
+        print("-" * 80)
     print("TMX file has been updated.")
 
 if __name__ == "__main__":
