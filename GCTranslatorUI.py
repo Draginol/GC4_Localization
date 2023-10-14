54c1bccebe254dd03c9985f7b72b86ca7240e52c
Draginol <bwardell@stardock.com>
2023-10-14 14:26:57 -0400

    Big Greekupdate

--- GCTranslatorUI.py
+++ GCTranslatorUI.py
@@ -373,11 +373,11 @@ class TranslationApp(QMainWindow):
             return row, translated_text
 
         # Split the rows into chunks
-        CHUNK_SIZE = 32
+        CHUNK_SIZE = 1
         chunks = [selected_rows[i:i + CHUNK_SIZE] for i in range(0, len(selected_rows), CHUNK_SIZE)]
 
         for chunk in chunks:
-            with ThreadPoolExecutor(max_workers=32) as executor:
+            with ThreadPoolExecutor(max_workers=1) as executor:
                 for idx, (row, translated_text) in enumerate(executor.map(translate_row, chunk), start=translation_counter):
                     translation_item = self.table.item(row, 4)
                     if not translation_item:
